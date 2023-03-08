d2e4e38c5b961a31613ff27c0b10852e66e65e66
Cristian Israel Martínez <101339211+israelmarti@users.noreply.github.com>
2023-03-08 16:55:19 -0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 setup(
   name = 'bisped',
   packages = ['bisped'], 
-  version = '1.01',
+  version = '1.1',
   description = 'Python script for binary spectroscopic disentangling',
   author = 'Cristian I. Martinez',
   author_email = 'cmartinez@conicet.gov.ar',
