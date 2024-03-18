91095255f4a616f752a605364c27a8eae441b294
Cristian Israel Martínez <101339211+israelmarti@users.noreply.github.com>
2024-03-18 17:31:01 -0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,12 +2,12 @@ from distutils.core import setup
 setup(
   name = 'bisped',
   packages = ['bisped'], 
-  version = '1.2',
+  version = '1.3',
   description = 'Python script for binary spectroscopic disentangling',
   author = 'Cristian I. Martinez',
   author_email = 'cmartinez@conicet.gov.ar',
   url = 'https://github.com/israelmarti/BiSpeD', # URL to the github repository
-  download_url = 'https://github.com/israelmarti/BiSpeD/tarball/v1.2', 
+  download_url = 'https://github.com/israelmarti/BiSpeD/tarball/v1.3', 
   keywords = ['astronomy', 'spectroscopy'], # keywords
   classifiers = [],
 )
