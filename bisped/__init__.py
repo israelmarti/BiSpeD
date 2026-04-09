2c323df70b660e2f93be3a820a39f79bf908d438
Cristian Israel Martínez <101339211+israelmarti@users.noreply.github.com>
2026-04-09 18:02:04 -0300

    Update __init__.py
    added onecomp function

--- bisped/__init__.py
+++ bisped/__init__.py
@@ -1,1 +1,1 @@
-from bisped.bisped import find2c,hselect,rvbina,rvextract,setrvs,spbina,splot,uniform,vgrid,vexplore,help
+from bisped.bisped import find2c,hselect,onecomp,rvbina,rvextract,setrvs,spbina,splot,uniform,vgrid,vexplore,help
