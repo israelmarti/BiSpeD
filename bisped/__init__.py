4a33245431ccc66f1fcee56870b12aa9e1e1a031
Cristian Israel Martínez <101339211+israelmarti@users.noreply.github.com>
2026-09-02 13:51:13 -0300

    Update __init__.py

--- bisped/__init__.py
+++ bisped/__init__.py
@@ -1,1 +1,34 @@
-from bisped.bisped import find2c,hselect,onecomp,rvbina,rvextract,setrvs,spbina,splot,uniform,vgrid,vexplore,help
+# bisped/__init__.py
+"""
+BiSpeD - Binary Spectral Disentangling
+"""
+
+from .bisped import (
+    find2c,
+    hselect,
+    onecomp,
+    qfitg,
+    rvbina,
+    rvextract,
+    setrvs,
+    spbina,
+    splot,
+    uniform,
+    vgrid,
+    vexplore
+)
+
+__version__ = "1.7"
+__all__ = [
+    'find2c',
+    'hselect',
+    'onecomp',
+    'qfitg',
+    'rvbina',
+    'setrvs',
+    'spbina',
+    'splot',
+    'uniform',
+    'vgrid',
+    'vexplore'
+]
